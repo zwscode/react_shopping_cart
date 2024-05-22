c5ea9c6a930efe5d8775d48681d18e40dc4d7cad
zws <zwssysu@gmail.com>
2024-05-22 09:16:22 -0500

    pagination

--- src/components/inventory/inventory.js
+++ src/components/inventory/inventory.js
@@ -1,13 +1,40 @@
 import React, { Component } from "react";
 import "./inventory.css";
 
+const ITEMS_PER_PAGE = 5;
+
 export default class Inventory extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            currentPage: 0,
+        };
+    }
+
+    handlePageChange = (change) => {
+        if (this.state.currentPage + change < 0) {
+            return;
+        } else if (
+            this.state.currentPage + change >
+            Math.ceil(this.props.inventory.length / ITEMS_PER_PAGE) - 1
+        ) {
+            return;
+        }
+        this.setState({ currentPage: this.state.currentPage + change });
+    };
+
     render() {
+        const getPageItems = () => {
+            const start = this.state.currentPage * ITEMS_PER_PAGE;
+            const end = start + ITEMS_PER_PAGE;
+            return this.props.inventory.slice(start, end);
+        };
+
         return (
             <div className="inventory-container">
                 <h1>Inventory</h1>
                 <div className="inventory-list">
-                    {this.props.inventory.map((item) => (
+                    {getPageItems().map((item) => (
                         <div className="inventory_item" key={item.id}>
                             <p className="inventory_name">{item.content}</p>
                             <button
@@ -43,6 +70,29 @@ export default class Inventory extends Component {
                             </button>
                         </div>
                     ))}
+                </div>
+                <div className="inventory-pagination">
+                    {this.state.currentPage > 0 ? (
+                        <button
+                            className="inv-prev-btn inventory_btns"
+                            onClick={() => this.handlePageChange(-1)}
+                        >
+                            prev
+                        </button>
+                    ) : null}
+
+                    <p id="page-label"> Page: {this.state.currentPage + 1}</p>
+
+                    {this.state.currentPage <
+                    Math.ceil(this.props.inventory.length / ITEMS_PER_PAGE) -
+                        1 ? (
+                        <button
+                            className="inv-next-btn inventory_btns"
+                            onClick={() => this.handlePageChange(1)}
+                        >
+                            next
+                        </button>
+                    ) : null}
                 </div>
             </div>
         );
